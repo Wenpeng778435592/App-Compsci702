10a35036eccf79478f16724f370fd2f399b613c2
Emily Zou <emilyzou@Mr.local>
2020-03-26 10:23:58 +1300

    added comments for timer

--- app/src/main/java/com/compsci702/compsci702app/Tools/MinuteTimer.java
+++ app/src/main/java/com/compsci702/compsci702app/Tools/MinuteTimer.java
@@ -1,5 +1,20 @@
 package com.compsci702.compsci702app.Tools;
 
+import android.widget.TextView;
+
 public class MinuteTimer {
+
+    //This is the text field in the UI that shows the time
+    TextView timerText;
+
+    public MinuteTimer(TextView timerText){
+        this.timerText = timerText;
+    }
+
+    //Starts the timer. Counts down from one minute (01:00). Sets the
+    //textField with the new time every second (01:00, 00:59, etc)
+    public void startTimer(){
+
+    }
 
 }
