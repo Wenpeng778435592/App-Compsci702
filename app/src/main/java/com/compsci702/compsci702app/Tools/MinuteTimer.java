3a97d14846f6a4729b62950962400e9cfb7aa468
Rahul Bagchi <rahul@matrixlan.co.nz>
2020-04-04 22:23:05 +1300

    update-databse

--- app/src/main/java/com/compsci702/compsci702app/Tools/MinuteTimer.java
+++ app/src/main/java/com/compsci702/compsci702app/Tools/MinuteTimer.java
@@ -1,6 +1,8 @@
 package com.compsci702.compsci702app.Tools;
+import android.os.CountDownTimer; //rb
 
 import android.widget.TextView;
+import android.widget.Toast; //rb
 
 public class MinuteTimer {
 
@@ -16,6 +18,13 @@ public class MinuteTimer {
     //textField with the new time every second (01:00, 00:59, etc)
     public void startTimer(){
         //rb start one minute timer
+        new CountDownTimer(60000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timerText.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+            public void onFinish() {
+                timerText.setText("done!");
+            }
+    }.start();
+}}
 
-    }
-}
--- app/src/main/java/com/compsci702/compsci702app/Tools/WordList.java
+++ app/src/main/java/com/compsci702/compsci702app/Tools/WordList.java
@@ -1,5 +1,9 @@
 package com.compsci702.compsci702app.Tools;
 
+
+import android.content.Context;
+import android.database.Cursor;
+import android.widget.Toast;
 import com.compsci702.compsci702app.Tools.DBHelper;
 
 import java.util.ArrayList;
@@ -17,8 +21,19 @@ public class WordList {
 
     private ArrayList<String> getWordListFromDatabase(int listLength){
         //rb /Dummy data
-        wordList.add("Word");
-        wordList.add("Jumper");
+        //rb read from database
+        Cursor cursor = dbHelper.alldata();
+        if (cursor.getCount() == 0){
+            wordList.add("no data");
+        }
+        else {
+            while (cursor.getCount() < (listLength + 1) && cursor.moveToNext()){
+              wordList.add(cursor.getString(1));
+            }
+        }
+        //Emily
+        //wordList.add("Word");
+        //wordList.add("Jumper");
         return wordList;
     }
 
